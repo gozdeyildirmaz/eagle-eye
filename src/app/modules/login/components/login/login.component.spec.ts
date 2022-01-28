718cf8d29f80b168316c45d8ca945cac602f2cd8
gozdeyildirmaz <gozdeyildirmaz@gmail.com>
2022-01-28 16:04:38 +0300

    -login unit test added

--- src/app/modules/login/components/login/login.component.spec.ts
+++ src/app/modules/login/components/login/login.component.spec.ts
@@ -1,6 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {RouterTestingModule} from '@angular/router/testing';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +13,18 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [
+        // RouterTestingModule,
+        HttpClientTestingModule,
+        BrowserModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatSnackBarModule,
+        RouterTestingModule
+      ],
+      declarations: [LoginComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +33,53 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render input for email', async(() => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#email')).toBeDefined()
+  }));
+
+  it('should render input for password', async(() => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#password')).toBeDefined()
+  }));
+
+
+  it('should render login button', async(() => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button').textContent).toContain('Login');
+  }));
+
+
+  it('should set initiial value', async(() => {
+    const loginFormGroup = component.userRegistrationForm;
+    const loginFormValues = {
+      email: '',
+      password: ''
+    }
+    expect(loginFormGroup.value).toEqual(loginFormValues)
+  }));
+
+
+
+
+  it('should valid when validation are fulfileed', () => {
+    const componentElement: HTMLElement = fixture.nativeElement;
+
+    fixture.detectChanges()
+    fixture.whenStable().then(()=>{
+      const  emailElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#email')
+      emailElement.value = 'sample@gmail.com';
+      emailElement.dispatchEvent(new  Event('input'))
+      expect(component.userRegistrationForm.controls['email'].value).toEqual('sample@gmail.com')
+    })
+
   });
+
+
 });
